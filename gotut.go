9c16e798a5005851f6da5134cecdcdbfec019d21
vinayb21 <vinay@hackerrank.com>
2019-05-19 11:09:10 +0530

    [Go] Use math package

--- gotut.go
+++ gotut.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("Welcome to Go!")
+	fmt.Println("The square root of 4 is", math.Sqrt(4))
+	fmt.Println("A number from 1-100", rand.Intn(100))
 }
